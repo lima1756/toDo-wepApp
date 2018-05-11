ed8c79b7334177e9c0fa8811c4caad19eb44ad26
Iván Morett <luisivanmorett@gmail.com>
2018-05-10 20:59:02 -0500

    AMP

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from './components/App';
 import history from './services/history'
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import '../node_modules/spectre.css/dist/spectre.min.css';
-import '../node_modules/spectre.css/dist/spectre-exp.min.css';
 import '../node_modules/spectre.css/dist/spectre-icons.min.css';
 
 require('./favicon.ico'); // Tell webpack to load favicon.ico
